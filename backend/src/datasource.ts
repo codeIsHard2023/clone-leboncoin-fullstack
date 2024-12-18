68d0bf4ad9a3f5acdc5954339866d87526c93889
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2024-12-18 15:23:21 +0100

    postgres db installation + docker db

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,8 +1,12 @@
 import { DataSource } from "typeorm";
 
 export const datasource = new DataSource({
-  type: "sqlite",
-  database: "./db/leboncoin.sqlite",
+  type: "postgres",
+  host: "db", // something like dnc that referencing physical adresse (can find in compose => services)
+  port: 5432,
+  username: "thegoodcorner",
+  password: "secretcorner",
+  database: "thegoodcorner",
   entities: ["./src/entities/*.ts"],
   synchronize: true,
   logging: true,
