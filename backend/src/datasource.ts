5f637a7896d9beaea8962ae3c3db10e891d19c9b
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2024-12-20 15:44:50 +0100

    adding env variables

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const datasource = new DataSource({
   type: "postgres",
-  host: "db", // something like dnc that referencing physical adresse (can find in compose => services)
+  host: "db", // something like dns that referencing physical adresse (can find in compose => services)
   port: 5432,
-  username: "thegoodcorner",
-  password: "secretcorner",
-  database: "thegoodcorner",
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ["./src/entities/*.ts"],
   synchronize: true,
   logging: true,
