f60a5168f596f36efb12762aef8ebbd784d0061e
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-01-16 15:35:24 +0100

    added: routes protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,67 @@ import { NewAdEditor } from "./pages/NewAdEditor";
 import { UpdateAdEditor } from "./pages/UpdateAdEditor";
 import { SignupPage } from "./pages/Signup";
 import { SigninPage } from "./pages/Signin";
+import { useQuery } from "@apollo/client";
+import { WHOAMI } from "./api/user";
 import "./App.css";
+import React from "react";
+
+enum AuthStates {
+  Authenticated,
+  Unauthenticated,
+}
+
+function authCheck(
+  Component: React.FC,
+  authStates: AuthStates[],
+  redirection: string = "/"
+) {
+  return function () {
+    const { data } = useQuery(WHOAMI);
+    const user = data?.whoAmI;
+
+    // if user is not detected
+    if (user === undefined) {
+      return null;
+    }
+    // check in what case component can be rendered
+    if (
+      // rendering of Signup and Signin pages
+      (user === null && authStates.includes(AuthStates.Unauthenticated)) ||
+      // for rendering others protected pages
+      (user && authStates.includes(AuthStates.Authenticated))
+    ) {
+      return <Component />;
+    }
+
+    return <Navigate to={redirection} replace />;
+  };
+}
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/signin" element={<SigninPage />} />
-        <Route path="about" element={<About />} />
-        <Route path="/ads/new-ad" element={<NewAdEditor />} />
-        <Route path="/ads/:id/update/" element={<UpdateAdEditor />} />
-        <Route path="/ads/:id" element={<AdDetails />} />
-        <Route path="/categories/:id" element={<CategoryAds />} />
+      <Route path="/" Component={DefaultLayout}>
+        <Route path="/" Component={Home} />
+        <Route
+          path="/signup"
+          Component={authCheck(SignupPage, [AuthStates.Unauthenticated])}
+        />
+        <Route
+          path="/signin"
+          Component={authCheck(SigninPage, [AuthStates.Unauthenticated])}
+        />
+        <Route path="about" Component={About} />
+        <Route
+          path="/ads/new-ad"
+          Component={authCheck(NewAdEditor, [AuthStates.Authenticated])}
+        />
+        <Route
+          path="/ads/:id/update/"
+          Component={authCheck(UpdateAdEditor, [AuthStates.Authenticated])}
+        />
+        <Route path="/ads/:id" Component={AdDetails} />
+        <Route path="/categories/:id" Component={CategoryAds} />
         <Route path="*" Component={() => <Navigate to="/" />} />
       </Route>
     </Routes>
